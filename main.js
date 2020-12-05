b9a8e396c85b75c14b45476e8ccb714b0347cee9
matttang27 <matttang27@gmail.com>
2020-12-04 19:06:46 -0500

    Added some functionality
    Totally not sus

---
+++ main.js
@@ -0,0 +1,44 @@
+var button = document.getElementById("e")
+var plus = document.getElementById("plus")
+var minus = document.getElementById("minus")
+var speed = 1000
+var show = document.getElementById("show")
+var change;
+
+console.log("test")
+
+button.addEventListener("click",() => {
+    console.log(button.innerHTML)
+    if (button.innerHTML == "stop") {
+        button.innerHTML = "click for epilepsy"
+        clearInterval(change);
+        document.body.style.backgroundColor = "black"
+        return;
+    }
+    else {
+        button.innerHTML = "stop"
+        change = setInterval(() => {
+            document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        },speed)
+    }
+    
+    
+})
+
+plus.addEventListener("click",() => {
+    speed += 50
+    show.innerHTML = `speed: ${speed}`
+    clearInterval(change);
+    change = setInterval(() => {
+        document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    },speed)
+})
+minus.addEventListener("click",() => {
+    speed -= 50
+    show.innerHTML = `speed: ${speed}`
+    clearInterval(change);
+    change = setInterval(() => {
+        document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    },speed)
+
+})
