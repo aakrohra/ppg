ea173838b9a93954541fdca0c230ddc4b14988cb
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2020-12-06 02:19:34 -0500

    made mai button toggle

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,21 +1,24 @@
-function hideImages() {
-    var gallery = document.getElementById("gallery")
-    gallery.remove();
-    gallery = document.createElement("div")
-    gallery.setAttribute("id", "gallery")
-    var place = document.getElementById("place")
-    document.body.insertBefore(gallery,place.nextSibling);
-}     
-
 function showImages() {
-    var gallery = document.getElementById("gallery")
-    console.log("test")
-    var images = []
-    for (i=1;i<=123;i++) {
-        images[i] = document.createElement("img");
-        images[i].setAttribute("src", `../resources/mai${i}.jpg`);
-        gallery.append(images[i])
-        console.log("set")
+    if (maibutton.value == "OFF") {
+        maibutton.value = "ON";
+        var gallery = document.getElementById("gallery")
+        console.log("test")
+        var images = []
+        for (i=1;i<=123;i++) {
+            images[i] = document.createElement("img");
+            images[i].setAttribute("src", `../resources/mai${i}.jpg`);
+            gallery.append(images[i])
+            console.log("set")
+        }
+    } 
+    else {
+        maibutton.value = "OFF";
+        var gallery = document.getElementById("gallery")
+        gallery.remove();
+        gallery = document.createElement("div")
+        gallery.setAttribute("id", "gallery")
+        var place = document.getElementById("place")
+        document.body.insertBefore(gallery,place.nextSibling);
     }
 }
 
