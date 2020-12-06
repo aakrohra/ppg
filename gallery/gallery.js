1f862831078373b9371f8b08b7ca88d50d674f27
matttang27 <matttang27@gmail.com>
2020-12-06 01:59:01 -0500

    added hide mai sakurajima command and added 100 more mai sakurajima pictures

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,19 +1,22 @@
 function hideImages() {
-    var images = []
-    for (i=1;i<=16;i++) {
-        gallery.remove(images);
-    }
+    var gallery = document.getElementById("gallery")
+    gallery.remove();
+    gallery = document.createElement("div")
+    gallery.setAttribute("id", "gallery")
+    var place = document.getElementById("place")
+    document.body.insertBefore(gallery,place.nextSibling);
 }     
 
 function showImages() {
     var gallery = document.getElementById("gallery")
     console.log("test")
     var images = []
-    for (i=1;i<=16;i++) {
+    for (i=1;i<=123;i++) {
         images[i] = document.createElement("img");
         images[i].setAttribute("src", `../resources/mai${i}.jpg`);
         gallery.append(images[i])
         console.log("set")
     }
 }
+
 
