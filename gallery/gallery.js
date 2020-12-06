4cc253971b86442f96f27cf8732c01fff54b7b87
matttang27 <matttang27@gmail.com>
2020-12-06 15:12:52 -0500

    Added 400 Mai Sakurajima images

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -5,7 +5,7 @@ function showImages() {
         var gallery = document.getElementById("gallery")
         console.log("test")
         var images = []
-        for (i=1;i<=123;i++) {
+        for (i=1;i<=486;i++) {
             images[i] = document.createElement("img");
             images[i].setAttribute("src", `../resources/mai${i}.jpg`);
             gallery.append(images[i])
