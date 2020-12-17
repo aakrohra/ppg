372ad32da5229cae27ebe07af8f903e86a333340
matttang27 <matttang27@gmail.com>
2020-12-17 18:58:56 -0500

    Example of setting parent css in mai.js

--- mai/mai.js
+++ mai/mai.js
@@ -17,57 +17,69 @@ imageY = []
 counter = 0
 
 function addImages() {
-    
+
     var maigallery = document.getElementById("maigallery")
-    for (i=counter*imagenum;i<(counter+1)*imagenum;i++) {
+    for (i = counter * imagenum; i < (counter + 1) * imagenum; i++) {
         if (i == 2407) {
             console.log(i)
             console.log("max reacher")
             break;
         }
         console.log(i)
-        
+
         var div = document.createElement("div");
         maigallery.append(div)
-        div.setAttribute("class","maiwrap")
-        
+        div.setAttribute("class", "maiwrap")
+
 
         images[i] = document.createElement("img");
         div.append(images[i])
         images[i].setAttribute("src", `../resources/mai${i}.jpg`);
-        images[i].setAttribute("id","mai" + i)
-        images[i].setAttribute("class","maiimg")
-    
+        images[i].setAttribute("id", "mai" + i)
+        images[i].setAttribute("class", "maiimg")
+
         var text = document.createElement("p");
-        text.setAttribute("class","maitext")
-        text.setAttribute("id","maitext" + i)
+        text.setAttribute("class", "maitext")
+        text.setAttribute("id", "maitext" + i)
         div.append(text)
         text.innerHTML = "Mai Sakurajima Best Girl"
     }
     counter++
-    maigallery.style.height = 340*(counter*imagenum)/Math.floor(width/320) + "px"
+    maigallery.style.height = 340 * (counter * imagenum) / Math.floor(width / 320) + "px"
+
+    //shyguy read!
+
+    //add hover function to maitext class
+    $('.maitext').hover(function () {
+        //selects parent and changes it's filter by 80 percent on hover
+        $(this).parent().css('filter', 'brightness(80%)');
+    },
+        function () {
+            //when release hover, change filter back to 100 percent.
+            $(this).parent().css('filter', 'brightness(100%)');
+        });
 }
 
 var height = $(document).height();
 var width = $(document).width();
-console.log(height,width)
+console.log(height, width)
 
-var height,width;
-$(window).resize(function() {
+var height, width;
+$(window).resize(function () {
     height = $(document).height();
     width = $(document).width();
     setup();
 });
 function setup() {
     var maigallery = document.getElementById("maigallery")
-    
-    maigallery.style.height = 340*(counter*imagenum)/Math.floor(width/320) + "px"
-    
+
+    maigallery.style.height = 340 * (counter * imagenum) / Math.floor(width / 320) + "px"
+
 }
 
-$(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
         console.log("add images")
         addImages()
     }
- });
+});
--- main.js
+++ main.js
@@ -1,60 +1,60 @@
-var button = document.getElementById("e");
-var plus = document.getElementById("plus");
-var minus = document.getElementById("minus");
-var speed = 1000;
-var show = document.getElementById("show");
-var change;
-var on = false;
+// var button = document.getElementById("e");
+// var plus = document.getElementById("plus");
+// var minus = document.getElementById("minus");
+// var speed = 1000;
+// var show = document.getElementById("show");
+// var change;
+// var on = false;
 var paragraph = document.getElementById("m");
 
 console.log("test");
 
-button.addEventListener("click", () => {
-    console.log(button.innerHTML)
-    if (button.innerHTML == "stop") {
-        button.innerHTML = "click for epilepsy"
-        clearInterval(change);
-        on = false
-        document.body.style.backgroundColor = "black"
-        return;
-    }
-    else {
-        button.innerHTML = "stop"
-        clearInterval(change);
-        on = true
-        change = setInterval(() => {
-            document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-        },speed)
-    }
+// button.addEventListener("click", () => {
+//     console.log(button.innerHTML)
+//     if (button.innerHTML == "stop") {
+//         button.innerHTML = "click for epilepsy"
+//         clearInterval(change);
+//         on = false
+//         document.body.style.backgroundColor = "black"
+//         return;
+//     }
+//     else {
+//         button.innerHTML = "stop"
+//         clearInterval(change);
+//         on = true
+//         change = setInterval(() => {
+//             document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+//         },speed)
+//     }
     
     
-})
+// })
 
-plus.addEventListener("click",() => {
-    speed += 50
-    show.innerHTML = `speed: ${speed}`
-    if (on) {
-        clearInterval(change);
-        change = setInterval(() => {
-            document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-        },speed)
-    }
+// plus.addEventListener("click",() => {
+//     speed += 50
+//     show.innerHTML = `speed: ${speed}`
+//     if (on) {
+//         clearInterval(change);
+//         change = setInterval(() => {
+//             document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+//         },speed)
+//     }
     
-})
+// })
 
 paragraph.innerHTML = "<span onclick=\"window.location.href='../mai/mai.html'\">Mai</span>tthew Tang";
 
-minus.addEventListener("click",() => {
-    speed -= 50
-    show.innerHTML = `speed: ${speed}`
-    if (on) {
-        clearInterval(change);
-        change = setInterval(() => {
-            document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-        },speed)
-    }
+// minus.addEventListener("click",() => {
+//     speed -= 50
+//     show.innerHTML = `speed: ${speed}`
+//     if (on) {
+//         clearInterval(change);
+//         change = setInterval(() => {
+//             document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+//         },speed)
+//     }
 
-})
+// })
 
 
 
