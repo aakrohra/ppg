f67d348b94953b7e46ce6b873082a25278d7127b
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2020-12-23 17:41:12 -0500

    trying (and failing) to make img clickable in gallery

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ console.log("test");
     
 // })
 
-paragraph.innerHTML = "<span onclick=\"window.location.href='../mai/mai.html'\">Mai</span>tthew Tang";
+paragraph.innerHTML = "<span onclick=\"window.location.href='../ppg/mai/mai.html'\">Mai</span>tthew Tang";
 
 // minus.addEventListener("click",() => {
 //     speed -= 50
