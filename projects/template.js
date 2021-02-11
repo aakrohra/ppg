54678ceaf8a48925d1b6ec7cb793461e00b42b17
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2021-02-11 01:55:28 -0500

    created template html/js files, uses query searching to replace template text for each individual project
    this took 3 hours and it is currently 2am and the code is a mess but absolutely worth it

---
+++ projects/template.js
@@ -0,0 +1,23 @@
+var title = document.getElementById("pptitle");
+var pname = document.getElementById("ppname");
+var desc = document.getElementById("ppdesc");
+
+var pageURL = window.location.href;
+var querySegment = pageURL.substr(pageURL.lastIndexOf('=') + 1);
+console.log(querySegment)
+
+$(document).ready(function () {
+    $.getJSON('data.json', function (data) {
+        for (var i = 0; i < data.length; i++){
+            if (data[i].code == querySegment){
+            title.innerHTML = (data[i].Title);
+            pname.innerHTML = (data[i].Name);
+            desc.innerHTML = (data[i].Description);
+            $("#ppvid").attr('src', data[i].Video);
+            }
+        }
+
+    });
+
+});
+
