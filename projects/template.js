b2e899dfd1173385349a66891bf680dd04bca230
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2021-02-26 14:52:27 -0500

    allows for multiple images; images smaller; video always on new line

--- projects/template.js
+++ projects/template.js
@@ -22,14 +22,30 @@ $(document).ready(function () {
                 image.setAttribute("src",data[i].Image)
                 template.append(image)
             }
+            if (data[i].hasOwnProperty('Image2') && data[i].Image2 != "N/A"){
+                var image2 = document.createElement("img")
+                image2.setAttribute("class","image")
+                image2.setAttribute("id","ppimg2")
+                image2.setAttribute("src",data[i].Image2)
+                template.append(image2)
+            }
+            if (data[i].hasOwnProperty('Image3') && data[i].Image3 != "N/A"){
+                var image3 = document.createElement("img")
+                image3.setAttribute("class","image")
+                image3.setAttribute("id","ppimg3")
+                image3.setAttribute("src",data[i].Image3)
+                template.append(image3)
+            }
             if (data[i].hasOwnProperty('Video') && data[i].Video != "N/A"){
+                var para = document.createElement("p")
                 var iframe = document.createElement("iframe")
                 iframe.setAttribute("class","video")
                 iframe.setAttribute("id","ppvid")
                 iframe.setAttribute("frameborder","0")
                 iframe.setAttribute("allowfullscreen","true")
                 iframe.setAttribute("src", data[i].Video)
-                template.append(iframe)
+                para.append(iframe)
+                template.append(para)
             }
 //            $("#ppvid").attr('src', data[i].Video);
 //            $("#ppimg").attr('src', data[i].Image);
