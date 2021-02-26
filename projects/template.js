e3b83431c09dd25e01d0bf9984d34f1b7c418849
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2021-02-26 13:29:43 -0500

    img + iframe only shows in individual template pages, if the field exists in json

--- projects/template.js
+++ projects/template.js
@@ -6,6 +6,8 @@ var pageURL = window.location.href;
 var querySegment = pageURL.substr(pageURL.lastIndexOf('=') + 1);
 console.log(querySegment)
 
+var template = document.getElementById("template")
+
 $(document).ready(function () {
     $.getJSON('data.json', function (data) {
         for (var i = 0; i < data.length; i++){
@@ -13,8 +15,23 @@ $(document).ready(function () {
             title.innerHTML = (data[i].Title);
             pname.innerHTML = (data[i].Name);
             desc.innerHTML = (data[i].Description);
-            $("#ppvid").attr('src', data[i].Video);
-            $("#ppimg").attr('src', data[i].Image);
+            if (data[i].hasOwnProperty('Image')){
+                var image = document.createElement("img")
+                image.setAttribute("class","image")
+                image.setAttribute("id","ppimg")
+                image.setAttribute("src",data[i].Image)
+                template.append(image)
+            }
+            if (data[i].hasOwnProperty('Video')){
+                var iframe = document.createElement("iframe")
+                iframe.setAttribute("class","video")
+                iframe.setAttribute("id","ppvid")
+                iframe.setAttribute("frameborder","0")
+                iframe.setAttribute("src", data[i].Video)
+                template.append(iframe)
+            }
+//            $("#ppvid").attr('src', data[i].Video);
+//            $("#ppimg").attr('src', data[i].Image);
             }
         }
 
